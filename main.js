7ab926e3aadba5477f367a354b40617b6ff9c0ff
Sally Kingston <sally.a.kingston@gmail.com>
2015-10-19 09:56:57 -0400

    fixed branching

--- main.js
+++ main.js
@@ -4,22 +4,28 @@ var startTree = function () {
   intervalID = window.setInterval(growTree, 2000);
 }
 
-var level = 1;
+var level = 0;
 var growTree = function() {
-    var branches = document.getElementsByClassName("branch");
+    var branches = document.getElementsByClassName("level"+level);
+    level++;
     Array.prototype.filter.call(branches, function(branch) {
         var newBranchLevel = document.createElement("ul");
         newBranchLevel = branch.appendChild(newBranchLevel);
+
         var branchLeft = document.createElement("li");
         branchLeft.innerHTML = "Branch";
+        branchLeft.classList.add("level"+level);
         branchLeft.classList.add("branch");
+
         var branchRight = document.createElement("li");
         branchRight.innerHTML = "Branch";
+        branchRight.classList.add("level"+level);
         branchRight.classList.add("branch");
+
         branchLeft = newBranchLevel.appendChild(branchLeft);
         branchRight = newBranchLevel.appendChild(branchRight);
 
-        level++;
+
         if (level === 10) {
             clearInterval(intervalID);
         }
